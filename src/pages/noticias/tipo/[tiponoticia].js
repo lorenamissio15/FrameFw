bd5860d1341ad96f609242c3fe9eff1e58db04ed
Lorena Missio <149403068+lorenamissio15@users.noreply.github.com>
2025-12-10 10:37:54 -0300

    Update [tiponoticia].js

--- src/pages/noticias/tipo/[tiponoticia].js
+++ src/pages/noticias/tipo/[tiponoticia].js
@@ -2,7 +2,7 @@ import { Container, Row } from 'react-bootstrap';
 import Cards from '../../components/cards';
 export async function getServerSideProps(req) {
   // Fetch data from external API
-  const res = await fetch('https://g3-snowy.vercel.app/noticias/tiponoticia/'+req.query.tiponoticia)
+  const res = await fetch('https://apiframe-eight.vercel.app/noticias/tiponoticia/'+req.query.tiponoticia)
   const repo = await res.json()
   // Pass data to the page via props
   return { props: { noticias: repo } }
